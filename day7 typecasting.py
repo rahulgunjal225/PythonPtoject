99ea1a72986e2f3b0c8c670ac9b0c87b875bb54b
rahulgunjal225 <rahulgunjal225@gmail.com>
2024-10-07 19:19:26 +0530

    Add files via upload

---
+++ day7 typecasting.py
@@ -0,0 +1,14 @@
+#the conversion of one data type into the other data type is known as type casting in python
+
+# alt+shift+down sign
+#type casting 
+#1=explicit conversion
+
+a="1"
+b="2"
+print(a+b)
+
+
+c="rahul"
+d="bro"
+print(c+d)
